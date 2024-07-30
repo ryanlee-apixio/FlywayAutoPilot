f0d3e0991ed6453fba8c151ab6a1189e4d363b93
Ryan Lee1~ <ryan.lee@apixio.com>
2024-07-30 09:42:20 -0500

    first push!✨

--- schema-model/Stored Procedures/dbo.GetAllWidgets.sql
+++ schema-model/Stored Procedures/dbo.GetAllWidgets.sql
@@ -2,12 +2,14 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
+ 
 CREATE   PROCEDURE [dbo].[GetAllWidgets]
 AS
 BEGIN
-	SELECT RecordID,
-           Description 
-	FROM Widgets
-END
+	SELECT w.RecordID,
+           w.Description ,
+		   wp.Price
+	FROM Widgets w INNER JOIN dbo.WidgetPrices wp 
+	ON wp.RecordID = w.RecordID
+   END
 GO
