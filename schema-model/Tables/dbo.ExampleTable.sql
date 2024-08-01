e6e2addb862528be1e2658e995f2c75e10780ed5
Bryan DeGregoria <bryan.degregoria@claimlogiq.com>
2024-08-01 14:16:05 -0400

    another column

--- schema-model/Tables/dbo.ExampleTable.sql
+++ schema-model/Tables/dbo.ExampleTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[ExampleTable]
 (
 [id] [int] NOT NULL IDENTITY(1, 1),
-[testcolumn] [varchar] (255) NULL
+[testcolumn] [varchar] (255) NULL,
+[anothercolumn] [varchar] (1) NULL
 )
 GO
